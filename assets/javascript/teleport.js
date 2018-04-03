2936fb395623ee7a05551180e63bd9b9b0d41d82
Madeleine Filloux <mfilloux@gmail.com>
2018-04-02 20:10:37 -0500

    get city info from teleport api on search

--- assets/javascript/teleport.js
+++ assets/javascript/teleport.js
@@ -1,16 +1,48 @@
-let queryURL = "https://api.teleport.org/api/cities/?search=san%20francisco";
+// variables to set on city search
+let cityName;
+let stateName;
+let countryName;
+let latLong = [];
+let population;
+let cityPhotoURL;
 
-$.ajax({
-    url: queryURL,
-    method: "GET",
-}).then((response) => {
-    console.log(response);
-});
+// listen for autocomplete on Teleport search widget
+TeleportAutocomplete.init('#teleport-autocomplete').on('change', function(response) {
+    const geoID = response.geonameId;
+    const queryURL = "https://api.teleport.org/api/cities/geonameid:" + geoID;
 
-// var $results = document.querySelector('.results');
-// var appendToResult = $results.insertAdjacentHTML.bind($results, 'afterend');
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    }).then((response) => {
+        console.log(response);
+        cityName = response.name;
+        stateName = response._links["city:admin1_division"].name;
+        countryName = response._links["city:country"].name;
+        latLong.push(response.location.latlon.latitude);
+        latLong.push(response.location.latlon.longitude);
+        population = response.population;
 
-TeleportAutocomplete.init('#teleport-autocomplete').on('change', function(value) {
-    console.log(value);
-//   appendToResult('<pre>' + JSON.stringify(value, null, 2) + '</pre>');
+        $("#search-city").text(`${cityName}, ${stateName}, ${countryName}`);
+        $("#city-info").append($("<p>").text(`Population: ${population}`));
+
+        if (response._links["city:urban_area"]) {
+            return $.ajax({
+                url: response._links["city:urban_area"].href,
+                method: "GET",
+            })
+        }
+    }).then((response) => {
+        console.log(response);
+        if (response) {
+            return $.ajax({
+                url: response._links["ua:images"].href,
+                method: "GET"
+            })
+        }
+    }).then((response) => {
+        console.log(response);
+        cityPhotoURL = response.photos[0].image.web;
+        $("#city-info").append($("<img>").attr("src", cityPhotoURL));
+    })
 });
