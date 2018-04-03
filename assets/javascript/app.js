4e1f8b9f5a8cc36c43c2a039f27e8ea3d4f2b943
adrianarmiller <adriana.rmiller@gmail.com>
2018-04-02 19:47:28 -0500

    Teleport info incorporated

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,21 +3,21 @@ $(document).ready(function(){
     let stateAbbrev;
 
     const weatherApiKey = "d94860da10767bf2";
+
+    var config = {
+        apiKey: "AIzaSyDtbDtrYHKNrdkYMqAmMAXxcHhIa4tLW-8",
+        authDomain: "relocation-app-bd038.firebaseapp.com",
+        databaseURL: "https://relocation-app-bd038.firebaseio.com",
+        projectId: "relocation-app-bd038",
+        storageBucket: "",
+        messagingSenderId: "37449828472"
+    };
+
+    firebase.initializeApp(config);
+
     
-    $("#submit").on("click", function(event){
-        event.preventDefault();
-        city = $("#search").val().trim();
-        
-        //state abbreviations to figure out through autocomplete teleport api
-        
-        //to all the apis
-        weatherApi();
-
-    });
-
     $("#weather").on("click", function(event) {
         weatherApi();
-
     })
 
     $("#food").on("click", function(event) {
@@ -30,7 +30,7 @@ $(document).ready(function(){
 
 
     function weatherApi(){
-        let queryURL = "http://api.wunderground.com/api/" + weatherApiKey + "/conditions/q/" + stateAbbrev + "/" + city + ".json";
+        let queryURL = "http://api.wunderground.com/api/" + weatherApiKey + "/conditions/q/" + stateName + "/" + cityName + ".json";
 
         let temp;
         let weather;
@@ -46,13 +46,14 @@ $(document).ready(function(){
             weather = response.current_observation.weather;        
         })
 
-        queryURL = "http://api.wunderground.com/api/" + weatherApiKey + "/webcams/q/" + stateAbbrev + "/" + city + ".json";
+        queryURL = "http://api.wunderground.com/api/" + weatherApiKey + "/webcams/q/" + stateName + "/" + cityName + ".json";
 
         //ajax call for webcam
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response) {
+            console.log(response);
             webcamUrl = response.webcams[1].CURRENTIMAGEURL;
         })
     }
