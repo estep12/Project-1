0047dc377874e39ba8f2e4e4d2b8404c06d9d277
hatab002 <hatab002@umn.edu>
2018-04-04 21:06:41 -0500

    Tweaking weather tab

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
     const weatherApiKey = "d94860da10767bf2";
 
     $("#weather-btn").on("click", function (event) {
+        $('#weather-add-on').empty();
         weatherApi();
     })
 
@@ -24,7 +25,7 @@ $(document).ready(function () {
         let avgLowC;
         let avgHighF;
         let avgHighC;
-
+        
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -50,7 +51,9 @@ $(document).ready(function () {
             $("#weather-add-on").append(weatherP);
             console.log(weatherP);
 
-            $("weather-add-on").append($("<img>").attr("src", iconUrl));
+            $("#weather-add-on").append($('<div><img id="icon"></div>'));
+            $('#icon').attr("src", iconUrl)
+            console.log(iconUrl);
         })
 
         queryURL = "http://api.wunderground.com/api/" + weatherApiKey + "/almanac/q/" + stateName + "/" + cityName + ".json";
