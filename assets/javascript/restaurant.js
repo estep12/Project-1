deec3d5e382179ac9cf5a01a994512a90ebd5af8
hatab002 <hatab002@umn.edu>
2018-04-05 14:13:10 -0500

    Food widget working with searched location, added some style to weather icon, commented repeated popouttab section

--- assets/javascript/restaurant.js
+++ assets/javascript/restaurant.js
@@ -1,12 +1,13 @@
 
 $('#food-btn').on("click", function foodWidget(){
     console.log(latLong)
-    let latitudeFood = latLong[0];
-    let longitudeFood = latLong[1];
+    let latitudeFood = latLong.lat;
+    let longitudeFood = latLong.lng;
 
     console.log(latitudeFood);
     console.log(longitudeFood);
     
-    $('#food-add-on').empty().append(newDiv);
-
+    // $('#food-add-on').empty().append(newDiv);
+    let newSrc ="https://www.zomato.com/widgets/res_search_widget.php?lat=" + latitudeFood + "&lon=" + longitudeFood + "&theme=red&hideCitySearch=on&hideResSearch=on&sort=rating"
+    $("iframe").attr("src", newSrc);
 })
