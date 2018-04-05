4b79127634f2fc76b22a5aac53faae03d430c776
hatab002 <hatab002@umn.edu>
2018-04-04 19:46:41 -0500

    working on jobs and restaurant tabs

--- assets/javascript/restaurant.js
+++ assets/javascript/restaurant.js
@@ -1,9 +1,11 @@
 
 $('#food-btn').on("click", function foodWidget(){
-    // let newDiv = $('<div class="widget_wrap" style="width:500px;height:450px;display:inline-block;"><iframe src="https://www.zomato.com/widgets/res_search_widget.php?city_id=826&theme=red&sort=popularity" style="position:relative;width:100%;height:100%;" border="0" frameborder="0"></iframe></div>')
-    let newCityName = $('#teleport-autocomplete').val().trim()
-    console.log(newCityName);
+    console.log(latLong)
+    let latitudeFood = latLong[0];
+    let longitudeFood = latLong[1];
+
+    console.log(latitudeFood);
+    console.log(longitudeFood);
     
-    // $('#food-add-on').append(newDiv);
-    $('.city_select>input>#city_input').val(newCityName);
+    $('#food-add-on').empty().append(newDiv);
 })
