c1a5c422f520fd425f715a781020b92559648801
Pam Pelkola <pelkola.pam@gmail.com>
2018-04-06 09:22:53 -0500

    added margin to Explore, font size for headings changed, removed about from css, font family changed to materialize default

--- assets/javascript/restaurant.js
+++ assets/javascript/restaurant.js
@@ -8,6 +8,6 @@ $('#food-btn').on("click", function foodWidget(){
     console.log(longitudeFood);
     
     // $('#food-add-on').empty().append(newDiv);
-    let newSrc ="https://www.zomato.com/widgets/res_search_widget.php?lat=" + latitudeFood + "&lon=" + longitudeFood + "&theme=red&hideCitySearch=on&hideResSearch=on&sort=rating"
+    let newSrc ="https://www.zomato.com/widgets/res_search_widget.php?lat=" + latitudeFood + "&lon=" + longitudeFood + "&theme=black&hideCitySearch=on&hideResSearch=on&sort=rating"
     $("iframe").attr("src", newSrc);
 })
