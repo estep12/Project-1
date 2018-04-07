ff7022a6b4f25f56d0f67eb6f3fa3556035a4766
Madeleine Filloux <mfilloux@gmail.com>
2018-04-06 20:42:28 -0500

    combined restaurant.js into teleport.js and populated jobs section

--- assets/javascript/restaurant.js
+++ assets/javascript/restaurant.js
@@ -1,13 +1,13 @@
 
-$('#food-btn').on("click", function foodWidget(){
-    console.log(latLong)
-    let latitudeFood = latLong.lat;
-    let longitudeFood = latLong.lng;
+// $('#food-btn').on("click", function foodWidget(){
+//     console.log(latLong)
+//     let latitudeFood = latLong.lat;
+//     let longitudeFood = latLong.lng;
 
-    console.log(latitudeFood);
-    console.log(longitudeFood);
+//     console.log(latitudeFood);
+//     console.log(longitudeFood);
     
-    // $('#food-add-on').empty().append(newDiv);
-    let newSrc ="https://www.zomato.com/widgets/res_search_widget.php?lat=" + latitudeFood + "&lon=" + longitudeFood + "&theme=black&hideCitySearch=on&hideResSearch=on&sort=rating"
-    $("iframe").attr("src", newSrc);
-})
+//     // $('#food-add-on').empty().append(newDiv);
+//     let newSrc ="https://www.zomato.com/widgets/res_search_widget.php?lat=" + latitudeFood + "&lon=" + longitudeFood + "&theme=black&hideCitySearch=on&hideResSearch=on&sort=rating"
+//     $("iframe").attr("src", newSrc);
+// })
